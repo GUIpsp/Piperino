1d65949ef9ec7dc27916825641aff5916d1d8592
Philip Hansen <me@philiphansen.dk>
2014-03-14 22:55:01 +0100

    Implement base mod file. No functionality, just shows up as loaded in-game.

---
+++ src/main/java/net/guipsp/piperino/Piperino.java
@@ -0,0 +1,31 @@
+package net.guipsp.piperino;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "piperino", name = "Piperino", version = "0.1")
+public class Piperino {
+
+    @Instance("piperino")
+    public static Piperino instance;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+
+}
