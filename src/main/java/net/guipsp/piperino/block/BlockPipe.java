6caf9ce024762aefe71cf38047ba8fcf7633ef8f
Philip Hansen <me@philiphansen.dk>
2014-04-07 14:48:36 +0200

    Pipe TE now stores array of adjecent pipes This array gets updated when the pipe is initially placed, and every time an adjecent block is changed

--- src/main/java/net/guipsp/piperino/block/BlockPipe.java
+++ src/main/java/net/guipsp/piperino/block/BlockPipe.java
@@ -2,47 +2,81 @@ package net.guipsp.piperino.block;
 
 import net.guipsp.piperino.reference.BlockInfo;
 import net.guipsp.piperino.tileentity.TileEntityPipe;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class BlockPipe extends BlockContainer {
-    protected BlockPipe() {
-        super(Material.iron);
-        setBlockName(BlockInfo.PIPE_NAME);
-        setCreativeTab(CreativeTabs.tabRedstone);
-    }
+	protected BlockPipe() {
+		super(Material.iron);
+		setBlockName(BlockInfo.PIPE_NAME);
+		setCreativeTab(CreativeTabs.tabRedstone);
+	}
 
-    @Override
-    public TileEntity createNewTileEntity(World var1, int var2) {
-        return new TileEntityPipe();
-    }
+	@Override
+	public TileEntity createNewTileEntity(World var1, int var2) {
+		return new TileEntityPipe();
+	}
 
-    @Override
-    public int getRenderType() {
-        return -1;
-    }
+	@Override
+	public int getRenderType() {
+		return -1;
+	}
 
-    @Override
-    public boolean renderAsNormalBlock() {
-        return false;
-    }
+	@Override
+	public boolean renderAsNormalBlock() {
+		return false;
+	}
 
-    @Override
-    public boolean isBlockNormalCube() {
-        return false;
-    }
+	@Override
+	public boolean isBlockNormalCube() {
+		return false;
+	}
 
-    @Override
-    public boolean isOpaqueCube() {
-        return false;
-    }
+	@Override
+	public boolean isOpaqueCube() {
+		return false;
+	}
 
-    @Override
-    public boolean isBlockSolid(IBlockAccess blockAccess, int x, int y, int z, int side) {
-        return false;
-    }
+	@Override
+	public boolean isBlockSolid(IBlockAccess blockAccess, int x, int y, int z, int side) {
+		return false;
+	}
+
+	@Override
+	public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase player, ItemStack itemStack) {
+		tryTileUpdate(world, x, y, z);
+	}
+
+	@Override
+	public void onNeighborBlockChange(World world, int x, int y, int z, Block block) {
+		tryTileUpdate(world, x, y, z);
+	}
+
+	private void tryTileUpdate(World world, int x, int y, int z) {
+		if (isServer(world)) {
+			if (hasTileEntityPipe(world, x, y, z)) {
+				updateBlockTileEntity(world, x, y, z);
+			}
+		}
+	}
+
+	private boolean isServer(World world) {
+		return !world.isRemote;
+	}
+
+	private boolean hasTileEntityPipe(World world, int x, int y, int z) {
+		return ((world.getTileEntity(x, y, z) != null) && (world.getTileEntity(x, y, z) instanceof TileEntityPipe));
+	}
+
+	private void updateBlockTileEntity(World world, int x, int y, int z) {
+		TileEntityPipe tileEntityPipe = (TileEntityPipe) world.getTileEntity(x, y, z);
+		tileEntityPipe.updatePipeArray();
+	}
 }
--- src/main/java/net/guipsp/piperino/tileentity/TileEntityPipe.java
+++ src/main/java/net/guipsp/piperino/tileentity/TileEntityPipe.java
@@ -1,7 +1,35 @@
 package net.guipsp.piperino.tileentity;
 
+import net.guipsp.piperino.block.BlockPipe;
+import net.minecraft.block.Block;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.common.util.ForgeDirection;
 
 public class TileEntityPipe extends TileEntity {
-    //TODO: Empty TE
+	private boolean[] nearbyPipes;
+
+	public TileEntityPipe() {
+		nearbyPipes = new boolean[6];
+	}
+
+	public void updatePipeArray() {
+		for (int i = 0; i < ForgeDirection.VALID_DIRECTIONS.length; i++) {
+			checkDirection(i);
+		}
+	}
+
+	private void checkDirection(int i) {
+		int[] coordinates = addOffset(xCoord, yCoord, zCoord, ForgeDirection.VALID_DIRECTIONS[i]);
+
+		Block block = worldObj.getBlock(coordinates[0], coordinates[1], coordinates[2]);
+		nearbyPipes[i] = isPipe(block);
+	}
+
+	private int[] addOffset(int x, int y, int z, ForgeDirection direction) {
+		return new int[]{x + direction.offsetX, y + direction.offsetY, z + direction.offsetZ};
+	}
+
+	private boolean isPipe(Block block) {
+		return (block instanceof BlockPipe);
+	}
 }
